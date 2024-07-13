cf176f25e8636f1008df514aeb57c523757e335b
Windisley <windisleyl@gmail.com>
2024-07-13 16:09:03 -0300

    code fim

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let horas = document.querySelector("#boxhoras p")
+let minutos = document.querySelector("#boxminutos p")
+let segundos = document.querySelector("#boxsegundos p")
+
+
+const relogio = () => {
+  let classdata = new Date();
+  horas.innerHTML = formatarnumero(classdata.getHours())
+  minutos.innerHTML = formatarnumero(classdata.getMinutes())
+  segundos.innerHTML = formatarnumero(classdata.getSeconds())
+};
+
+function formatarnumero(numero) {
+  return numero < 10 ? "0" + numero : numero
+}
+
+setInterval(relogio, 900)
